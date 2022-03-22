62f7701eff0f7b23b7db5dda4078c61cf34ebbe5
Gustavo Santos <Gustavo.Santos2@br.ey.com>
2022-03-22 18:05:02 -0300

    primeiro commit

---
+++ src/test/java/br/com/junitmockito/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.junitmockito.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
